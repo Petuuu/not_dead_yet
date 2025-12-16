b880e2e24ea3bdcfbd5f0efd40032f6d0f9c67a1
Petu <pojp07@gmail.com>
2025-12-16 23:53:00 +0200

    feat(backend): add database

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,51 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from typing import Annotated
+from sqlalchemy.orm import Session
+from db import engine, SessionLocal
+import models
+import datetime
 
 app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
 
 origins = ["http://localhost:3000"]
 
 app.add_middleware(
     CORSMiddleware, allow_origins=origins, allow_methods=["*"], allow_headers=["*"]
 )
+
+
+class CourseBase(BaseModel):
+    name: str
+
+
+class DeadlineBase(BaseModel):
+    course: int
+    name: str
+    due: datetime.datetime
+
+
+class TaskBase(BaseModel):
+    course: int
+    deadline: int
+    todo: str
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@app.post("/courses/")
+async def add_course(course: CourseBase, db: db_dependency):
+    db_course = models.Courses(name=course.name)
+    db.add(db_course)
+    db.commit()
---
+++ backend/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from db import Base
+
+
+class Courses(Base):
+    __tablename__ = "courses"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+
+
+class Deadlines(Base):
+    __tablename__ = "deadlines"
+
+    id = Column(Integer, primary_key=True, index=True)
+    course = Column(Integer, ForeignKey("courses.id"))
+    name = Column(String)
+    due = Column(DateTime)
+
+
+class Tasks(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True)
+    course = Column(Integer, ForeignKey("courses.id"))
+    deadline = Column(Integer, ForeignKey("deadlines.id"))
+    todo = Column(String)
