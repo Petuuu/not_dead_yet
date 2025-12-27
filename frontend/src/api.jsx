27a3155e8fc7aed10994378e97488a1d41964dfd
Petu <pojp07@gmail.com>
2025-12-27 12:10:21 +0200

    change api url

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const isDev = window.location.hostname === 'localhost';
-const baseURL = isDev ? "http://localhost:8000" : process.env.API_URL;
+const baseURL = isDev ? "http://localhost:8000" : process.env.REACT_APP_API_URL;
 
 const api = axios.create({
     baseURL: baseURL
